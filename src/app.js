19fb0251bd7b5efcda753e7dc2e39c3c9e5a2ce4
Jon Eastin <picardncce@gmail.com>
2022-07-13 10:48:32 -0700

    finished 31.2

--- src/app.js
+++ src/app.js
@@ -1,17 +1,32 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const pastes = require('./data/pastes-data')
+const app = express()
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
+app.use('/pastes', (req, res) => {
+	res.json({ data: pastes })
+})
+
+app.use('/pastes/:pasteId', (req, res, next) => {
+	const { pasteId } = req.params
+	const foundPaste = pastes.find((paste) => paste.id === Number(pasteId))
+
+	if (foundPaste) {
+		res.json({ data: foundPaste })
+	} else {
+		next(`Paste id not found: ${pasteId}`)
+	}
+})
 
 // Not found handler
 app.use((request, response, next) => {
-  next(`Not found: ${request.originalUrl}`);
-});
+	next(`Not found: ${request.originalUrl}`)
+})
 
 // Error handler
 app.use((error, request, response, next) => {
-  console.error(error);
-  response.send(error);
-});
+	console.error(error)
+	response.send(error)
+})
 
-module.exports = app;
+module.exports = app
