af364015a9b40f6180cd4bf62ecefe31bac8984a
Jon Eastin <picardncce@gmail.com>
2022-07-14 17:09:46 -0700

    finished 31.6

--- src/app.js
+++ src/app.js
@@ -2,44 +2,12 @@ const express = require('express')
 const pastes = require('./data/pastes-data')
 const app = express()
 
+const pastesRouter = require('./pastes/pastes.router')
+
 app.use(express.json())
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
-app.get('/pastes', (req, res) => {
-	res.json({ data: pastes })
-})
-
-// New middleware function to validate the request body
-function bodyHasTextProperty(req, res, next) {
-	const { data: { text } = {} } = req.body
-	if (text) {
-		return next() // Call `next()` without an error messaage if the result exists
-	}
-	next({
-		status: 400,
-		message: 'A "text" property is required.',
-	})
-}
-
-// Variable to hold the next ID
-// Because some IDs may already be used, find the largest assigned ID
-let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0)
-
-app.post('/pastes', bodyHasTextProperty, (req, res) => {
-	const { data: { name, syntax, exposure, expiration, text, user_id } = {} } =
-		req.body
-	const newPaste = {
-		id: ++lastPasteId, // Increment last ID, then assign as the current ID
-		name: name,
-		syntax: syntax,
-		exposure: exposure,
-		expiration: expiration,
-		text: text,
-		user_id: user_id,
-	}
-	pastes.push(newPaste)
-	res.status(201).json({ data: newPaste })
-})
+app.use('/pastes', pastesRouter) // Note: app.use
 
 app.use('/pastes/:pasteId', (req, res, next) => {
 	const { pasteId } = req.params
---
+++ src/pastes/pastes.controller.js
@@ -0,0 +1,148 @@
+const pastes = require('../data/pastes-data')
+
+function list(req, res) {
+	res.json({ data: pastes })
+}
+
+// New middleware function to validate the request contains all fields
+function bodyDataHas(propertyName) {
+	return function (req, res, next) {
+		const { data = {} } = req.body
+		if (data[propertyName]) {
+			return next() // Call `next()` without an error messaage if the result exists
+		}
+		next({ status: 400, message: `Must include a ${propertyName}` })
+	}
+}
+
+function exposurePropertyIsValid(req, res, next) {
+	const { data: { exposure } = {} } = req.body
+	const validExposure = ['private', 'public']
+	if (validExposure.includes(exposure)) {
+		return next()
+	}
+	next({
+		status: 400,
+		message: `Value of the 'exposure' property must be one of ${validExposure}. Received ${exposure}`,
+	})
+}
+
+function syntaxPropertyIsValid(req, res, next) {
+	const { data: { syntax } = {} } = req.body
+	const validSyntax = [
+		'None',
+		'JavaScript',
+		'Python',
+		'Ruby',
+		'Perl',
+		'C',
+		'Scheme',
+	]
+	if (validSyntax.includes(syntax)) {
+		return next()
+	}
+	next({
+		status: 400,
+		message: `alue of the 'syntax' property must be one of ${validSyntax}. Received: ${syntax}`,
+	})
+}
+
+function expirationIsValidNumber(req, res, next) {
+	const { data: { expiration } = {} } = req.body
+	if (expiration <= 0 || !Number.isInteger(expiration)) {
+		return next({
+			status: 400,
+			message: `Expiration requires a valid number`,
+		})
+	}
+	next()
+}
+
+// Variable to hold the next ID
+// Because some IDs may already be used, find the largest assigned ID
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0)
+
+function create(req, res) {
+	const { data: { name, syntax, exposure, expiration, text, user_id } = {} } =
+		req.body
+	const newPaste = {
+		id: ++lastPasteId, // Increment last ID, then assign as the current ID
+		name,
+		syntax,
+		exposure,
+		expiration,
+		text,
+		user_id,
+	}
+	pastes.push(newPaste)
+	res.status(201).json({ data: newPaste })
+}
+
+function pasteExists(req, res, next) {
+	const { pasteId } = req.params
+	const foundPaste = pastes.find((paste) => paste.id === Number(pasteId))
+	if (foundPaste) {
+		return next()
+	} else {
+		next({ status: 404, message: `Paste id not found: ${pasteId}` })
+	}
+}
+
+function read(req, res) {
+	const { pasteId } = req.params
+	const foundPaste = pastes.find((paste) => paste.id === Number(pasteId))
+	res.json({ data: foundPaste })
+}
+
+function update(req, res) {
+	const { pasteId } = req.params
+	const foundPaste = pastes.find((paste) => paste.id === Number(pasteId))
+	const { data: { name, syntax, expiration, exposure, text } = {} } = req.body
+
+	// Update the paste
+	foundPaste.name = name
+	foundPaste.syntax = syntax
+	foundPaste.expiration = expiration
+	foundPaste.exposure = exposure
+	foundPaste.text = text
+
+	res.json({ data: foundPaste })
+}
+
+function destroy(req, res) {
+	const { pasteId } = req.params
+	const index = pastes.findIndex((paste) => paste.id === Number(pasteId))
+	// `splice()` returns an array of the deleted elements, even if it is one element
+	const deletedPastes = pastes.splice(index, 1)
+	res.sendStatus(204)
+}
+
+module.exports = {
+	create: [
+		bodyDataHas('name'),
+		bodyDataHas('text'),
+		bodyDataHas('syntax'),
+		bodyDataHas('user_id'),
+		bodyDataHas('exposure'),
+		bodyDataHas('expiration'),
+		exposurePropertyIsValid,
+		syntaxPropertyIsValid,
+		expirationIsValidNumber,
+		create,
+	],
+	list,
+	read: [pasteExists, read],
+	update: [
+		pasteExists,
+		bodyDataHas('name'),
+		bodyDataHas('syntax'),
+		bodyDataHas('exposure'),
+		bodyDataHas('expiration'),
+		bodyDataHas('text'),
+		exposurePropertyIsValid,
+		syntaxPropertyIsValid,
+		expirationIsValidNumber,
+		update,
+	],
+	delete: [pasteExists, destroy],
+}
